8f4ae5598b14509f6bb86d81080c4823c04d1a23
Rosiele David <rosieleserpa@hotmail.com>
2023-02-05 11:41:53 -0300

    criando primeiras rotas

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,6 +1,16 @@
 import http from 'node:http' // utiliza-se o node: para informar que é um módulo interno do node
 
 const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res.end('Listagem de usuários')
+  }
+
+  if (method === 'POST' && url === '/users') {
+    return res.end('Criação de usuário')
+  }
+
   return res.end('Hello World')
 })
 
