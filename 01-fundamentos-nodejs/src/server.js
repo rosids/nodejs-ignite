c1bafdacd47c94541712738464d16132ede36b99
Rosiele David <rosieleserpa@hotmail.com>
2023-02-05 12:05:43 -0300

    explicação de stateful e stateless

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,5 +1,10 @@
 import http from 'node:http' // utiliza-se o node: para informar que é um módulo interno do node
 
+/* 
+stateful -> dados salvos em memória, ao reiniciar o servidor os dados são perdidos.
+stateless -> dados persistentes, ou seja, salvos em fontes externas.
+  Ex: banco de dados ou arquivo de texto.
+*/
 const users = []
 
 const server = http.createServer((req, res) => {
