cca61c8ce54dc920b9161c8839f717a455d526b8
Rosiele David <rosieleserpa@hotmail.com>
2023-02-05 11:58:52 -0300

    add status code para rotas inexistentes

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -21,7 +21,7 @@ const server = http.createServer((req, res) => {
     return res.writeHead(201).end()
   }
 
-  return res.end('Hello World')
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
