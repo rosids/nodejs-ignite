5e2730d7ea6a4a24194e8001f8eae350ef523b6d
Rosiele David <rosieleserpa@hotmail.com>
2023-02-05 11:51:08 -0300

    salvando usuários em memória

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,13 +1,22 @@
 import http from 'node:http' // utiliza-se o node: para informar que é um módulo interno do node
 
+const users = []
+
 const server = http.createServer((req, res) => {
   const { method, url } = req
 
   if (method === 'GET' && url === '/users') {
-    return res.end('Listagem de usuários')
+    return res
+      .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@example.com'
+    })
+
     return res.end('Criação de usuário')
   }
 
