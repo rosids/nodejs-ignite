16874935e27cdaf418acb116355c65df1576ec28
Rosiele David <rosieleserpa@hotmail.com>
2023-02-11 11:31:03 -0300

    separando rotas da aplicação

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,7 +1,7 @@
 import http from 'node:http' // utiliza-se o node: para informar que é um módulo interno do node
-import { randomUUID } from 'node:crypto'
-import { Database } from './database.js'
+
 import { json } from './middlewares/json.js'
+import { routes } from './routes.js'
 
 /* 
 stateful -> dados salvos em memória, ao reiniciar o servidor os dados são perdidos.
@@ -9,31 +9,17 @@ stateless -> dados persistentes, ou seja, salvos em fontes externas.
   Ex: banco de dados ou arquivo de texto.
 */
 
-const database = new Database()
-
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
   await json(req, res)
 
-  if (method === 'GET' && url === '/users') {
-    const users = database.select('users')
+  const route = routes.find(route => {
+    return route.method === method && route.path === url
+  })
 
-    return res.end(JSON.stringify(users))
-  }
-
-  if (method === 'POST' && url === '/users') {
-    const { name, email } = req.body
-
-    const user = {
-      id: randomUUID(),
-      name,
-      email
-    }
-
-    database.insert('users', user)
-
-    return res.writeHead(201).end()
+  if (route) {
+    return route.handler(req, res)
   }
 
   return res.writeHead(404).end()
