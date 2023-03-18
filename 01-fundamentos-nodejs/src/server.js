6531429679b602cce210366e295d8ad687c378af
Rosiele David <rosieleserpa@hotmail.com>
2023-03-18 10:33:23 -0300

    explicação de Query Parameters, Route Parameters e Request Body

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -7,6 +7,17 @@ import { routes } from './routes.js'
 stateful -> dados salvos em memória, ao reiniciar o servidor os dados são perdidos.
 stateless -> dados persistentes, ou seja, salvos em fontes externas.
   Ex: banco de dados ou arquivo de texto.
+
+3 formas de quem está consumindo a API enviar as informações para o backend
+Query Parameters: URL Stateful => enviado na própria URL
+  Utilizado em filtros, paginação, não obrigatórios
+  http:localhost:3333/users?userId=1
+Route Parameters: Identificação de recurso. Parâmetro na rota não nomeado
+  GET http:localhost:3333/users/1
+  DELETE http:localhost:3333/users/1
+Request Body: enviado no corpo da requisição. (Passam pelo HTTPs)
+  Envio de informações de um formulário
+  POST http:localhost:3333/users
 */
 
 const server = http.createServer(async (req, res) => {
