d3a5fe6d46774024607d40f7cb12d0b0e51f68fa
Rosiele David <rosieleserpa@hotmail.com>
2023-03-18 11:06:06 -0300

    rotas com parâmetros (RegEx)

--- 01-fundamentos-nodejs/src/routes.js
+++ 01-fundamentos-nodejs/src/routes.js
@@ -1,13 +1,14 @@
 import { randomUUID } from 'node:crypto'
 
 import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
 
 const database = new Database()
 
 export const routes = [
   {
     method: 'GET',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
       const users = database.select('users')
 
@@ -16,7 +17,7 @@ export const routes = [
   },
   {
     method: 'POST',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
       const { name, email } = req.body
 
@@ -31,4 +32,11 @@ export const routes = [
       return res.writeHead(201).end()
     }
   },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      return res.writeHead(200).end()
+    }
+  }
 ]
--- 01-fundamentos-nodejs/src/utils/build-route-path.js
+++ 01-fundamentos-nodejs/src/utils/build-route-path.js
@@ -4,5 +4,12 @@ export function buildRoutePath(path) {
   // O g define que é uma regex global e irá procurar por todos os matches daquela operação
   const routeParametersRegex = /:([a-zA-Z]+)/g
 
-  console.log(Array.from(path.matchAll(routeParametersRegex)))
+  // ?<> -> define um nome do grupo para regex
+  // $1 -> retorno da posição 1 da regex, neste caso da regex routeParametersRegex
+  const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
+
+  // ^ -> indica que começa daquela forma
+  const pathRegex = new RegExp(`^${pathWithParams}`)
+
+  return pathRegex
 }
