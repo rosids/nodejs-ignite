926b9e115007a3d2c554e05891ae8c2762e3c809
Rosiele David <rosieleserpa@hotmail.com>
2023-04-14 16:58:35 -0300

    02 - configurando eslint

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,4 +1,4 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const app = fastify()
 
@@ -6,8 +6,10 @@ app.get('/hello', () => {
   return 'Hello World!'
 })
 
-app.listen({
-  port: 3333
-}).then(() => {
-  console.log('HTTP Server Running!')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
