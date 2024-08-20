4d2fb88e764303731e0a886ebdd41dbe63f71d6d
Rosiele David <rosieleserpa@hotmail.com>
2024-08-20 20:47:54 -0300

    03 - Outros controllers da academia

--- 03-api-solid/README.md
+++ 03-api-solid/README.md
@@ -123,7 +123,7 @@ Desenvolvido utilizando o node v18.16.1
 - [x] Teste E2E da autenticação
 - [x] Teste E2E do perfil
 - [x] Controller de criação de academia
-- [ ] Outros controllers da academia
+- [x] Outros controllers da academia
 - [ ] Controllers das rotas de check-ins
 - [ ] Testes E2E de rotas de academias
 - [ ] Testes E2E de rotas de check-ins
--- 03-api-solid/src/http/controllers/gyms/routes.ts
+++ 03-api-solid/src/http/controllers/gyms/routes.ts
@@ -2,8 +2,17 @@ import { FastifyInstance } from 'fastify'
 
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
+import { search } from './search'
+import { nearby } from './nearby'
+import { create } from './create'
+
 export async function gymsRoutes(app: FastifyInstance) {
   // todas rotas abaixo do addHook irão chamar o verifyJWT
   // hook no fastify equivale ao middleware
   app.addHook('onRequest', verifyJWT)
+
+  app.get('/gyms/search', search)
+  app.get('/gyms/nearby', nearby)
+
+  app.post('/gyms', create)
 }
